d8c843fd04b39d935ab0ee42ff64ab4dbebfb938
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2017-02-27 09:31:43 -0300

    Port to sdl2

--- src/Nxt/Audio.hs
+++ src/Nxt/Audio.hs
@@ -4,23 +4,26 @@ module Nxt.Audio
      loadMusic,
      playMusic) where
 
-import qualified Graphics.UI.SDL.Mixer.General as SDL.Mixer
-import qualified Graphics.UI.SDL.Mixer.Music as SDL.Mixer.Music
-import qualified Graphics.UI.SDL.Mixer.Types as SDL.Mixer.Types
+import qualified SDL.Mixer
 
-type Music = SDL.Mixer.Types.Music
+type Music = SDL.Mixer.Music
 
 -- initAudio
 initAudio :: IO ()
-initAudio = SDL.Mixer.openAudio 44100 SDL.Mixer.AudioS16Sys 2 4096
+initAudio = 
+    let audio = SDL.Mixer.Audio
+          { SDL.Mixer.audioFrequency = 44100
+          , SDL.Mixer.audioFormat = SDL.Mixer.FormatS16_Sys
+          , SDL.Mixer.audioOutput = SDL.Mixer.Stereo }
+    in SDL.Mixer.openAudio audio 4096
 
 -- loadMusic
 loadMusic :: String -> IO Music
-loadMusic = SDL.Mixer.Music.loadMUS
+loadMusic = SDL.Mixer.load
 
 -- playMusic
 playMusic :: Music -> IO ()
 playMusic m = do
-    SDL.Mixer.Music.setMusicVolume 50
-    SDL.Mixer.Music.playMusic m (-1)
+    SDL.Mixer.setMusicVolume 50
+    SDL.Mixer.playMusic SDL.Mixer.Forever m
 
